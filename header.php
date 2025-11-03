0bd52e07ee02a89996fc9f228ea1374a396eaa94
shjang <jangs4339@gmail.com>
2025-11-04 01:00:35 +0900

    feat: header / footer 생성

---
+++ header.php
@@ -0,0 +1,23 @@
+<?php 
+require "./asset/db/dbconnect.php";
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Mangrove Solution</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:site_name" content="Mangrove Solution" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Mangrove Solution" />
+        <meta property="og:description" content="We are Mangrove" />
+        <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="asset/css/main.css" />
+    </head>
+    <body>
+<?php
+session_start();
+// $name = $_SESSION['name'];
+?>
