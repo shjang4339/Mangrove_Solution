32492dbe75f66a6ae495aab627f7e7e9522ecdc9
shjang <jangs4339@gmail.com>
2025-11-04 00:58:39 +0900

    feat: DB 정보 업데이트

---
+++ asset/db/dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    // 'server'=>'localhost',
+    // 'name'=>'mangrove_solution',
+    // 'username'=>'mangrove',
+    // 'password'=>'mangrove1110!',
+    // 'charset'=>'utf8'
+    'server'=>'localhost',
+    'name'=>'mangrove_solution',
+    'username'=>'root',
+    'password'=>'',
+    'charset'=>'utf8'
+];
+?>
